3c05259dc82077d6ad84488b4e7e1e0c5fd0d0b0
Jayk <jay.khavadia@rlogical.com>
2024-11-07 16:05:00 +0530

    - added pages

---
+++ app/lib/actions.ts
@@ -0,0 +1,78 @@
+"use server";
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  try {
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get("customerId"),
+      amount: formData.get("amount"),
+      status: formData.get("status"),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split("T")[0];
+    console.log("🚀 ~  file: actions.ts:22 ~  createInvoice ~  date:", date);
+    const res = await sql`
+          INSERT INTO invoices (customer_id, amount, status, date)
+          VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    revalidatePath("/ui/dashboard/invoices");
+    redirect("/ui/dashboard/invoices");
+  } catch (error) {
+    console.error(
+      "🚀 ~  file: actions.ts:35 ~  createInvoice ~  error:",
+      error
+    );
+    throw error;
+  }
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  try {
+    const { customerId, amount, status } = UpdateInvoice.parse({
+      customerId: formData.get("customerId"),
+      amount: formData.get("amount"),
+      status: formData.get("status"),
+    });
+
+    const amountInCents = amount * 100;
+
+    await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+          `;
+
+    revalidatePath("/ui/dashboard/invoices");
+    redirect("/ui/dashboard/invoices");
+  } catch (error) {
+    console.error(
+      "🚀 ~  file: actions.ts:55 ~  updateInvoice ~  error:",
+      error
+    );
+    throw error;
+  }
+}
+
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath("/ui/dashboard/invoices");
+  } catch (error) {
+    console.error("file: actions.ts:60 ~  deleteInvoice ~  error:", error);
+    throw error;
+  }
+}
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -142,23 +142,30 @@ export async function fetchInvoicesPages(query: string) {
 
 export async function fetchInvoiceById(id: string) {
   try {
-    const data = await sql<InvoiceForm>`
-      SELECT
-        invoices.id,
-        invoices.customer_id,
-        invoices.amount,
-        invoices.status
-      FROM invoices
-      WHERE invoices.id = ${id};
-    `;
+    try {
+      const data = await sql<InvoiceForm>`
+        SELECT
+          invoices.id,
+          invoices.customer_id,
+          invoices.amount,
+          invoices.status
+        FROM invoices
+        WHERE invoices.id = ${id};
+      `;
 
-    const invoice = data.rows.map((invoice) => ({
-      ...invoice,
-      // Convert amount from cents to dollars
-      amount: invoice.amount / 100,
-    }));
-
-    return invoice[0];
+      const invoice = data.rows.map((invoice) => ({
+        ...invoice,
+        // Convert amount from cents to dollars
+        amount: invoice.amount / 100,
+      }));
+      return invoice[0];
+    } catch (error) {
+      console.error(
+        "🚀 ~  file: data.ts:163 ~  fetchInvoiceById ~  error:",
+        error
+      );
+    }
+    return undefined;
   } catch (error) {
     console.error("Database Error:", error);
     throw new Error("Failed to fetch invoice.");
